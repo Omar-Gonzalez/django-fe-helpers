c143b4de3101af91c41fb863fcf29b810af4e512
Omar Gonzalez <gonzalezrocha.omar@gmail.com>
2017-12-27 20:50:19 -0600

    AJX helper refactor

--- js-src/lib/ajax-helpers.js
+++ js-src/lib/ajax-helpers.js
@@ -1,9 +1,10 @@
-let API = window.API || {};
+let AJX = window.AJX || {};
 
-API.html = class {
+AJX.html = class {
     /**
     * Append / Replace html within the document with AJAX Call
-    * @parms {url:"..",sel:"div selector"}
+    * @param {url:"..",sel:"div selector"}
+    * @param cb:(optional) done callback will execute after call
     * Static Methods:
     * API.html.append (appends new html to existing) 
     * API.html.replace (swaps html for new one)
@@ -18,19 +19,19 @@ API.html = class {
         }
     }
 
-    static append(options) {
-        API.html._validate(options);
+    static append(options,cb) {
+        this._validate(options);
         options.action = "APPEND";
-        API.html._GET(options);
+        this._GET(options, cb ? cb :null);
     }
 
-    static replace(options) {
-        API.html._validate(options);
+    static replace(options,cb) {
+        this._validate(options);
         options.action = "HTML";
-        API.html._GET(options);
+        this._GET(options, cb ? cb : null);
     }
 
-    static _GET(opt) {
+    static _GET(opt,cb) {
         jQuery.ajax({
                 url: opt.url,
                 type: "GET",
@@ -48,6 +49,14 @@ API.html = class {
             })
             .always(function() {
                 /* ... */
+                if(!cb){
+                    return;
+                }
+                if(typeof cb === "function"){
+                    cb();
+                }else{
+                    throw("API.html on done call back must be a function");
+                }
             });
     }
 };
